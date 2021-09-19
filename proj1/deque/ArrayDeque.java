ba10c5d7f9cbc39475992f9adef81edede8f2570
cliff-bielinski <clifftacopter@gmail.com>
2021-09-18 21:25:37 -0700

    AD constructor made

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,4 +1,25 @@
 package deque;
 
-public class ArrayDeque {
+public class ArrayDeque <T>{
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private T[] items;
+
+    /** Initializes an ArrayDeque object */
+    public ArrayDeque() {
+        size = 0;
+        items = (T[]) new Object[8];
+        nextFirst = 3;
+        nextLast = 4;
+    }
+
+    /** adds an element to the start of the ArrayDeque */
+    public void addFirst(T item){
+        if (size == items.length){
+            //RESIZE CALL HERE resize();
+        }
+        items[nextFirst] = item;
+        nextFirst = (nextFirst - 1) % size;
+    }
 }
